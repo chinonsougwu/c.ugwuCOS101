d7cf6eb9645ddb04c41e5400c54a4e3a89cbf476
chinonsougwu <ugwuchinonsojames@gmail.com>
2023-12-13 01:14:11 +0100

    week-9

---
+++ week9/practice_2/target/debug/deps/practice_2.d
@@ -0,0 +1,5 @@
+C:\Users\ugwuj\OneDrive\Documents\c.ugwuCOS101\week9\practice_2\target\debug\deps\practice_2.exe: src\main.rs
+
+C:\Users\ugwuj\OneDrive\Documents\c.ugwuCOS101\week9\practice_2\target\debug\deps\practice_2.d: src\main.rs
+
+src\main.rs:
---
+++ week9/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\ugwuj\OneDrive\Documents\c.ugwuCOS101\week9\practice_2\target\debug\practice_2.exe: C:\Users\ugwuj\OneDrive\Documents\c.ugwuCOS101\week9\practice_2\src\main.rs
